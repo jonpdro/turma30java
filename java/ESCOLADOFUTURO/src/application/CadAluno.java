da9dc2a282c0150a07d044d130eb07803f4fd204
João P. Cruz <jpcgomes0@gmail.com>
2021-08-05 18:00:14 -0300

    Desafio ED

--- java/ESCOLADOFUTURO/src/application/CadAluno.java
+++ java/ESCOLADOFUTURO/src/application/CadAluno.java
@@ -5,57 +5,84 @@ import java.util.Scanner;
 public class CadAluno {
 
 	public static void main(String[] args) {
-
 		Scanner leia = new Scanner(System.in);
 
-		String alunes[] = { "Ana Beatriz Yujra Espejo", "Ana Carolina Gonzalez de Souza", "Ana Paula Souza Dias",
-				"Anderson Coelho da Costa", "Andrei Felipe Corrêa de Souza", "Ariel de Barros Pirangy Soares",
-				"Beatriz dos Santos Teixeira", "Beatriz Gomes de Abreu", "Caio Saldanha Motta",
-				"Cássia de Arruda Nicolau Santos", "Emerson da Silva Santana", "Enzo Fulaneto",
-				"Ester Gomes Neves Nascimento", "Fábio Campaner Suzuki", "Felipe Matos de Lima",
-				"Felipe Souza da Silva", "Flávio Augusto da Costa", "Giovanna Siqueira Paiva dos Penedos",
+		String[] alunes = { "Ana Beatriz Yujra Espejo", "Ana Carolina G. de Souza", "Ana Paula Souza Dias",
+				"Anderson Coelho da Costa", "Andrei Felipe C. de Souza", "Ariel B. Pirangy Soares",
+				"Beatriz dos Santos Teixeira", "Beatriz Gomes de Abreu", "Caio Saldanha Motta     ",
+				"Cássia de Arruda N. Santos", "Emerson da Silva Santana", "Enzo Fulaneto          ",
+				"Ester Gomes N. Nascimento", "Fábio Campaner Suzuki", "Felipe Matos de Lima",
+				"Felipe Souza da Silva", "Flávio Augusto da Costa", "Giovanna S. P. Penedos",
 				"Gustavo Mesquita Ferreira", "Henrique Uriel Lopes", "João Pedro Cruz Gomes",
-				"José Paulo de Matos Ferreira Neto", "Letícia Porto Martins", "Lucas Silva Nunes de Aguiar",
-				"Maicon Gomes Cerqueira", "Marcos Vinicius Coutinho Rego", "Matheus de Araujo Farina",
-				"Matheus de Brito Milani", "Natália Regina dos Santos Rocha", "Pamela Paulino",
-				"Renata dos Santos Ferreira", "Rodrigo Roseo Lopes da Costa", "Sabrina Alves de Paiva",
-				"Sergio de Jesus Severo", "Stefani Fernanda Pereira Tosi",
-				"Talita gleice maria da gloria da Silva Lima", "Thiago da Silva Machado", "Vinícios Lisboa da Silva",
-				"Vinicius Cardoso Siqueira Francisco" };
-		int[] notas = new int[alunes.length], novaNota;
-		String[] matriculas = new String[alunes.length], matriculaEscolhida; // MAT-0X
+				"José Paulo de Matos F. Neto", "Letícia Porto Martins", "Lucas Silva Nunes de Aguiar",
+				"Maicon Gomes Cerqueira", "Marcos Vinicius C. Rego", "Matheus de Araujo Farina",
+				"Matheus de Brito Milani", "Natália Regina S. Rocha", "Pamela Paulino          ",
+				"Renata dos Santos Ferreira", "Rodrigo Roseo L. Costa", "Sabrina Alves de Paiva",
+				"Sergio de Jesus Severo", "Stefani Fernanda P. Tosi",
+				"Talita Gleice M. G. S. Lima", "Thiago da Silva Machado", "Vinícios Lisboa da Silva",
+				"Vinicius C. S. Francisco" };
+		int[] notas = new int[alunes.length];
+		int novaNota;
+		String[] matriculas = new String[alunes.length]; // MAT-0X
+		String matriculaEscolhida;
 		boolean[] ativo = new boolean[alunes.length];
 		char resposta;
+		String atividadeAlune;
 
 		for (int x = 0; x < alunes.length; x++) {
 			ativo[x] = true;
-
 			if (x >= 9) {
 				matriculas[x] = "MAT-" + (x + 1);
 			} else {
 				matriculas[x] = "MAT-0" + (x + 1);
 			}
-		}
 
+		}
+		System.out.println("MATRICULA   ALUNO\t\t\t NOTA\tSTATUS");
 		for (int x = 0; x < alunes.length; x++) {
-			System.out.println(matriculas[x] + "\t" + alunes[x]);
+			if(ativo[x] == true) {
+				System.out.println(matriculas[x] +"\t    "+ alunes[x] + "\t   " + notas[x] + "\tAtivo");
+			}
 		}
-
 		System.out.println();
 		
 		do {
-			System.out.println("Digite uma matrícula: ");
-			System.out.println("Digite a nota desse aluno: ");
-			System.out.println("Digite se o aluno está ativo ou inativo: ");
-			System.out.println("Continuar? [S/N]: ");
+			System.out.print("DIGITE A MATRICULA DO ALUNE: ");
+			matriculaEscolhida = leia.next().toUpperCase();
+			
+			for (int x = 0; x < alunes.length; x++) {
+				if (matriculaEscolhida.equals(matriculas[x])) {
+					System.out.println("MATRICULA   ALUNO\t\t\t NOTA\tSTATUS");
+					System.out.println(matriculas[x] +"\t    "+ alunes[x] + "\t   " + notas[x] + "\tAtivo");
+					System.out.print("\nDIGITE A NOTA DO ALUNE: ");
+					novaNota = leia.nextInt();
+					notas[x] = novaNota;
+
+					System.out.println("QUAL O STATUS DO ALUNE [ATIVO/ INATIVO]: ");
+					atividadeAlune = leia.next().toUpperCase();
+
+					if (atividadeAlune.equals("ATIVO")) {
+						ativo[x] = true;
+						
+					} else if (atividadeAlune.equals("INATIVO")) {
+						ativo[x] = false;
+						
+					} else {
+						System.out.println("STATUS INVÁLIDO, SITUAÇÃO PERMANECE A MESMA!");
+					}
+
+				}
+			}
+			System.out.print("Continuar? [S/N]: ");
 			resposta = leia.next().toUpperCase().charAt(0);
+			
 		} while (resposta == 'S');
 
 		for (int x = 0; x < alunes.length; x++) {
 			if (notas[x] <= 5) {
 				System.out.println("Estude mais");
 			} else if (notas[x] > 5) {
-
+				System.out.println("ótimo, continue assim!");
 			}
 		}
 
