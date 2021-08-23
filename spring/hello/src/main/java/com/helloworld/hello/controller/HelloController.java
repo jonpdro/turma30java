b3a6cb14d7fe1ba8a54ea4ef90636f4908d7991a
João P. Cruz <jpcgomes0@gmail.com>
2021-08-23 17:04:57 -0300

    Spring Calculadora

--- spring/hello/src/main/java/com/helloworld/hello/controller/HelloController.java
+++ spring/hello/src/main/java/com/helloworld/hello/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.helloworld.hello.controller;
 
+import java.util.Scanner;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
-
+	
 	@GetMapping
-	public String hello() {
-		return "Hello Generation";
+	public String calculadora() {
+		Scanner ler = new Scanner(System.in);
+		int numA, numB, soma, subtr, divisao, multi;
+		
+		System.out.print("Insira o número A: ");
+		numA = ler.nextInt();
+		
+		System.out.print("Insira o número B: ");
+		numB = ler.nextInt();
+		
+		soma = numA + numB;
+		subtr = numA - numB;
+		divisao = numA / numB;
+		multi = numA * numB;
+		
+		return "Hello World! -> A soma: "+ soma + ". A subtração: " + subtr + ". A divisão: "+ divisao + ". A multiplicação: "+ multi + ".";
 	}
 }
